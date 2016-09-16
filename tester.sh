fa78036521ee058b7ad8803486c38867bb53d76d
hippyjake <hippyjake@gmail.com>
2016-09-16 12:16:12 -0700

    Tester script thing

--- tester.sh
+++ tester.sh
@@ -2,10 +2,10 @@
 
 
 #colors
-red=`tput setaf 1`
-green=`tput setaf 2`
-yellow=`tput setaf 3`
-reset=`tput sgr0`
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+reset=$(tput sgr0)
 
 echo "${green} _____ __    _____ _____"
 echo "|     |  |  |  |  |   __|"
@@ -14,7 +14,7 @@ echo "|_____|_____|_____|_____|${reset}"
 
 echo "${yellow}Exporting path${reset}"
 
-rbversion=`ruby --version | awk {'print substr($2, 0, 5)'}`
+rbversion=$(ruby --version | awk '{print substr($2, 0, 4)}' | awk '$1=$1"0"')
 case "$rbversion" in
 
 	$rbversion)
@@ -23,20 +23,20 @@ case "$rbversion" in
 	;;
 	$rbversion)
 	echo "$rbversion path set"
-	export PATH=~/.gem/ruby/$rbversion/bin:$PATH
+	export PATH=~/.gem/ruby/"$rbversion"/bin:$PATH
 	;;
 	*)
-	echo $rbversion
+	echo "$rbversion"
 	;;
 
 esac
 
 echo "${yellow}Checking Dependencies${reset}"
-if [ -f ~/.gem/ruby/$rbversion/bin/bundler ]
+if [ -f ~/.gem/ruby/"$rbversion"/bin/bundler ]
 	then
 	echo "${green}Found Bundler${reset}"
 
-elif [ -f ~/.gem/ruby/$rbversion/bin/bundler ]
+elif [ -f ~/.gem/ruby/"$rbversion"/bin/bundler ]
 	then
 	echo "${green}Found Bundler${reset}"
 
@@ -51,7 +51,7 @@ if [ -d vendor ]
 	then
 	echo "${green}Found vendor folder${reset}"
 	echo "${green}Updating Dependencies${reset}"
-	#bundle update
+	bundle install --path vendor
 else
 	echo "${red}Installing Dependencies${reset}"
 	bundle install --path vendor
@@ -65,7 +65,6 @@ if [ -d _site ]
 	echo "${green}Done${reset}"
 else
 	echo "${green}All set${reset}"
-
 fi
 
 echo "${green}Opening Browser Tab...${reset}"
